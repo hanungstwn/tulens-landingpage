73e651062351e55c814a27c285399972246d9a3b
hanungstwn <hanungsetiawan23@gmail.com>
2023-02-28 17:02:15 +0700

    mit komit

--- js/script.js
+++ js/script.js
@@ -27,3 +27,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   }
 });
+function prev() {
+  document.getElementById("slider-container").scrollLeft -= 270;
+}
+
+function next() {
+  document.getElementById("slider-container").scrollLeft += 270;
+}
+
+$(".slide img").on("click", function () {
+  $(this).toggleClass("zoomed");
+  $(".overlay").toggleClass("active");
+});
